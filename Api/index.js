6d0ba4adb216b4788ad01c0dcd0a5abcfb46c4d1
Oldcast <dexieskull@gmail.com>
2025-12-10 14:45:32 +0700

    update 21

--- Api/index.js
+++ Api/index.js
@@ -1,21 +1,12 @@
-import express from "express";
 import fetch from "node-fetch";
-import cors from "cors";
 import { generateLangCard } from "./svg.js";
 
-const app = express();
-app.use(cors());
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).send("Method Not Allowed");
+  }
 
-const TOKEN = process.env.GITHUB_TOKEN;
-
-if (!TOKEN) {
-  console.warn("WARNING: GITHUB_TOKEN is not defined!");
-}
-
-app.get("/api/top-langs", async (req, res) => {
-  const username = req.query.username;
-  const token = req.query.token;
-  const theme = req.query.theme || "radical";
+  const { username, token, theme = "radical" } = req.query;
 
   if (!username) {
     return res.status(400).send("username is required");
@@ -27,12 +18,12 @@ app.get("/api/top-langs", async (req, res) => {
     const repos = await fetch(
       `https://api.github.com/users/${username}/repos?per_page=100&type=all`,
       { headers }
-    ).then((r) => r.json());
+    ).then(r => r.json());
 
     let languageStats = {};
 
     for (const repo of repos) {
-      const langs = await fetch(repo.languages_url, { headers }).then((r) =>
+      const langs = await fetch(repo.languages_url, { headers }).then(r =>
         r.json()
       );
 
@@ -45,20 +36,14 @@ app.get("/api/top-langs", async (req, res) => {
       .sort((a, b) => b[1] - a[1])
       .slice(0, 4);
 
-    const top4Object = Object.fromEntries(top4);
-
-    const svg = await generateLangCard({ languages: top4Object }, theme);
+    const svg = await generateLangCard(
+      { languages: Object.fromEntries(top4) },
+      theme
+    );
 
     res.setHeader("Content-Type", "image/svg+xml");
     return res.send(svg);
   } catch (err) {
-    return res.status(500).send("Something went wrong: " + err.message);
+    return res.status(500).send("Error: " + err.message);
   }
-});
-
-app.get("/", (_, res) => {
-  res.send("GitHub Stats API is running");
-});
-
-// IMPORTANT: Vercel must receive this
-export default app;
+}
